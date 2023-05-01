5d6a617b16a6160f37eebee03fd902944f641fe6
Princezer <127698134+Princezer@users.noreply.github.com>
2023-05-02 11:45:21 +1200

    the start of the game
    game start

--- code box.py
+++ code box.py
@@ -2,5 +2,9 @@
 Rules = input("Welcome To the Te Reo Maori Quiz\n"
       "would you like the rules?:").lower()
 if Rules == "y" or Rules == "yes":
-    print ("Rules")
+
+
+    print ("Here are the rules")
+
+
 print ("the game")
