debb4d95dcc875759e8ac30394d2979e3fd38086
Princezer <127698134+Princezer@users.noreply.github.com>
2023-05-03 12:05:37 +1200

    Dont Bother With Code Box
    dont bother with code box
    its just the box where i add the code

--- code box.py
+++ code box.py
@@ -4,7 +4,19 @@ Rules = input("Welcome To the Te Reo Maori Quiz\n"
 if Rules == "y" or Rules == "yes":
 
 
-    print ("Here are the rules")
+    print ("x"*40)
+    print ("These are the rules.\n"
+           "You will be asked 10 questions,\n"
+           "some will be mutli-choice and some will be written.\n"
+           "for multi-choice aswers, you will be given 4 options,\n"
+           "'A','B','C', and 'D'.\n"
+           "enter the letter as an answer not the word.\n"
+           "for the written questions you will be asked a question.\n"
+           "the answer will be a single word.\n"
+           "(pls note that if you spell the word wrong it will be marked as inccorect)\n"
+           "at the end of the game you will get a score out of ten.\n\n"
+           "HAVE FUN")
+    print ("x"*40)
 
 
 print ("the game")
