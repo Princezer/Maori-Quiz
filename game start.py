743aa5df136bcaa68b210c31d01dec9bb51148ce
Princezer <127698134+Princezer@users.noreply.github.com>
2023-05-03 12:03:31 +1200

    repost

---
+++ game start.py
@@ -0,0 +1,10 @@
+
+Rules = input("Welcome To the Te Reo Maori Quiz\n"
+      "would you like the rules?:").lower()
+if Rules == "y" or Rules == "yes":
+
+
+    print ("Here are the rules")
+
+
+print ("the game")
